225081f8df420ae2c0b577feaf67f8d30f1a9fe5
shu4 <shu4@hawaii.edu>
2024-03-25 23:05:09 -1000

    sw unreg

---
+++ _scripts/unsw.js
@@ -0,0 +1,14 @@
+if ('serviceWorker' in navigator) {
+    // Unregister service worker named 'sw.js'
+    navigator.serviceWorker.getRegistrations().then(function(registrations) {
+        registrations.forEach(function(registration) {
+            if (registration.active && registration.active.scriptURL.endsWith('sw.js')) {
+                registration.unregister().then(function(success) {
+                    console.log('Service worker unregistered:', registration);
+                }).catch(function(error) {
+                    console.error('Failed to unregister service worker:', error);
+                });
+            }
+        });
+    });
+}
